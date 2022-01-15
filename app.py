9f025d28433f26458415488bb5b0d66ae546dbb0
Akash R Prabhu <akashprabhu41@gmail.com>
2022-01-15 16:07:22 +0530

    before adding announcement functions

--- app.py
+++ app.py
@@ -29,39 +29,19 @@ def logout():
     return render_template("index.html",id=None)
 # table creation
 
-# create table UserEmail(
-# email_id varchar(50) primary key, password varchar(16) not null);
 class UserEmail(db.Model):
     email_id=db.Column(db.String(50),primary_key=True)
     password=db.Column(db.String(200),nullable=False)
 
-# create table UserIdentity( UniqueID varchar(12) primary key, user_name varchar(50) not null, mobile int unique not null);
 class UserIdentity(db.Model):
     UniqueID =db.Column(db.String(50),primary_key=True)
     user_name=db.Column(db.String(200),unique=True,nullable=False)
     mobile=db.Column(db.Integer,unique=True,nullable=False)
 
-
-# create table Users(
-# id int primary key,
-# email_id varchar(50) unique not null,
-# UniqueID varchar(12) unique not null,
-# foreign key (email_id) references UserEmail(email_id),
-# foreign key (UniqueID) references UserIdentity(UniqueID));
 class users(db.Model,UserMixin):
     id =db.Column(db.String(10),primary_key=True)
     email_id=db.Column(db.String(50),db.ForeignKey(UserEmail.email_id),nullable=False)
     UniqueID=db.Column(db.String(50),db.ForeignKey(UserIdentity.UniqueID),unique=True,nullable=False)
-
-
-# create table UserAddress(
-# id int primary key,
-# DoorNo varchar(10) not null,
-# Street varchar(100) not null,
-# Area varchar(100) not null,
-# City varchar(50) not null,
-# State varchar(50) not null,
-# Pincode numeric(6,0) not null, foreign key (id) references Users(id));
 
 class UserAddress(db.Model):
     id =db.Column(db.String(10),db.ForeignKey(users.id),primary_key=True)
@@ -72,74 +52,40 @@ class UserAddress(db.Model):
     State=db.Column(db.String(50),nullable=False)
     Pincode=db.Column(db.String(6),nullable=False)
 
-# create table Admins(
-# admin_id int primary key,
-# admin_desig varchar(30) not null,
-# foreign key (admin_id) references Users(id));
 class Admins(db.Model):
     admin_id=db.Column(db.String(10),db.ForeignKey(users.id),primary_key=True)
     admin_desig=db.Column(db.String(30),nullable=False)
 
-# create table NGO( 
-# NGO_id int primary key, 
-# foreign key (NGO_id) references Users(id)); 
 class NGO(db.Model):
     NGO_id=db.Column(db.String(10),db.ForeignKey(users.id),primary_key=True)
-  
-# create table Donor( 
-# donor_id int primary key, 
-# dob date not null, 
-# foreign key (donor_id) references Users(id)); 
+
 class Donor(db.Model):
     donor_id=db.Column(db.String(10),db.ForeignKey(users.id),primary_key=True)
     dob=db.Column(db.String(15),nullable=False)
  
    
-
-# create table Requestor( 
-# requestor_id int primary key, 
-# dob date not null, 
-# foreign key (requestor_id) references Users(id));
 class Requestor(db.Model):
     requestor_id=db.Column(db.String(10),db.ForeignKey(users.id),primary_key=True)
     dob=db.Column(db.String(15),nullable=False)
 
-# create table BlockedUsers( donor_id int primary key, block_reason varchar(20) not null, block_date date not null, admin_id int not null,
-# foreign key (donor_id) references Donor(donor_id), foreign key (admin_id) references Admins(admin_id));
 class BlockedUsers(db.Model):
     donor_id=db.Column(db.String(10),db.ForeignKey(Donor.donor_id),primary_key=True)
     block_reason=db.Column(db.String(20),nullable=False)
     block_date=db.Column(db.String(15),nullable=False)
     admin_id=db.Column(db.Integer,db.ForeignKey(Admins.admin_id),nullable=False)
 
-# create table Medicine(
-# med_id int primary key,
-# med_name varchar(100) unique not null);
 class Medicine(db.Model):
     med_id=db.Column(db.Integer,primary_key=True)
     med_name=db.Column(db.String,unique=True,nullable=False)
 
-# create table MedicineEffects(
-# med_id int not null,
-# med_effects varchar(50) not null,
-# primary key(med_id, med_effects),
-# foreign key (med_id) references Medicine(med_id));
 class MedicineEffects(db.Model):
     med_id=db.Column(db.Integer,db.ForeignKey(Medicine.med_id),nullable=False,primary_key=True)
     med_effects=db.Column(db.String(50),nullable=False,primary_key=True)
 
-# create table MedicineClass(
-# med_id int not null,
-# med_class varchar(50) not null,
-# primary key (med_id, med_class),
-# foreign key (med_id) references Medicine(med_id));
 class MedicineClass(db.Model):
     med_id=db.Column(db.Integer,db.ForeignKey(Medicine.med_id),nullable=False,primary_key=True)
     med_class=db.Column(db.String(50),nullable=False,primary_key=True)
 
-# create table RequestedMedicine( request_id int primary key, med_id int not null,
-# need_date date not null, requested_date date not null, requested_quanity int not null, requestor_id int not null,
-# foreign key (requestor_id) references Requestor(requestor_id), foreign key (med_id) references Medicine(med_id));
 class RequestedMedicine(db.Model):
     request_id=db.Column(db.Integer,primary_key=True)
     med_id=db.Column(db.Integer,db.ForeignKey(Medicine.med_id),nullable=False)
@@ -148,8 +94,6 @@ class RequestedMedicine(db.Model):
     requested_quantity=db.Column(db.Integer,nullable=False)
     requestor_id=db.Column(db.Integer,db.ForeignKey(Requestor.requestor_id),nullable=False)
 
-# create table DonatedMedicine( donation_id int primary key, med_id int not null, donation_expiry date not null, donation_quantity int not null, donor_id int not null,
-# foreign key(med_id) references Medicine(med_id), foreign key(donor_id) references Donor(donor_id));
 class DonatedMedicine(db.Model):
     donation_id=db.Column(db.Integer,primary_key=True)
     med_id=db.Column(db.Integer,db.ForeignKey(Medicine.med_id),nullable=False)
@@ -157,13 +101,6 @@ class DonatedMedicine(db.Model):
     donation_quantity=db.Column(db.Integer,nullable=False)
     donor_id=db.Column(db.Integer,db.ForeignKey(Donor.donor_id),nullable=False)
 
-# create table DeliveredMedicine(
-# delivery_id int primary key,
-# request_id int unique not null,
-# delivery_date date not null,
-# delivered_quantity int not null,
-# donation_id int unique not null,
-# foreign key (request_id) references RequestedMedicine(request_id), foreign key (donation_id) references DonatedMedicine(donation_id));
 class DeliveredMedicine(db.Model):
     delivery_id=db.Column(db.Integer,primary_key=True)
     request_id=db.Column(db.Integer,db.ForeignKey(RequestedMedicine.request_id),unique=True,nullable=False)
@@ -171,36 +108,11 @@ class DeliveredMedicine(db.Model):
     delivered_quantity=db.Column(db.Integer,nullable=False)
     donation_id=db.Column(db.Integer,db.ForeignKey(DonatedMedicine.donation_id),unique=True,nullable=False)
 
-
-# create table Announcement(
-# request_id int primary key,
-# foreign key (request_id) references RequestedMedicine(request_id));
 class Announcement(db.Model):
     request_id=db.Column(db.Integer,db.ForeignKey(RequestedMedicine.request_id),primary_key=True)
 
 
 # Table creation completed
-
-# @app.route("/")
-# def hello_world():
-#     return render_template("index.html")
-
-
-# @app.route("/user_registration",methods=['GET','POST'])
-# def user_registration():
-#     ruid=0
-#     if request.method=="POST":
-#         uid=random.randint(1000,9999)
-#         ruid=uid
-#         upass=request.form['pass']
-#         ufname=request.form['fname']
-#         ulname=request.form['lname']
-#         umobile=request.form['phone']
-#         uaadhar=request.form['aadharno']
-#         users = users_db(uid=uid,ufname=ufname,ulname=ulname,umobile=umobile,uaadhar=uaadhar,upass=upass)
-#         db.session.add(users)
-#         db.session.commit()
-#     return render_template("user_registration.html",ruid=ruid)
 
 
 @app.route("/")
@@ -444,13 +356,6 @@ def requestor_homepage():
     else:
         return render_template("forbidden.html")
 
-# class RequestedMedicine(db.Model):
-#     request_id=db.Column(db.Integer,primary_key=True)
-#     med_id=db.Column(db.Integer,db.ForeignKey(Medicine.med_id),nullable=False)
-#     need_date=db.Column(db.DateTime,nullable=False)
-#     requested_date=db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
-#     requested_quanity=db.Column(db.Integer,nullable=False)
-#     requestor_id=db.Column(db.Integer,db.ForeignKey(Requestor.requestor_id),nullable=False)
 
 @app.route("/ngo_homepage", methods=['GET', 'POST'])
 @login_required
@@ -491,6 +396,7 @@ def admin_homepage():
     id=str(current_user.get_id())
     if id[:3]=="ADM":
         if request.method=="POST":
+            #uncomment below lines to add accept and reject
             # if request.form['identifier']=="form1":
             #     request_id=request.form['request_id']
             #     if request.form['submit_button']=="accept":
@@ -592,12 +498,7 @@ def admin_homepage():
         count_requestor=len(Requestor.query.all())
         count_blocked=len(blocked_list)
         count_ngo=len(NGO.query.all())
-        del_list=DeliveredMedicine.query.all()
-        # total=count_donor+count_requestor+count_ngo
-        # perc_donor=( (count_donor-count_blocked)/total)*100
-        # perc_requestor=count_requestor*100/total
-        # perc_blocked=count_blocked*100/total
-        # perc_ngo=count_ngo*100/total
+        del_list=DeliveredMedicine.query.all()    
         admin_details=users.query.filter_by(id=id)  
         l=list()
         for i in del_list:
